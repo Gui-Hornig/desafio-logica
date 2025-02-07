7d49531425757e2589e5fce5b525839f8fa02a1b
Gui-hornig <guihornig@gmail.com>
2025-02-07 13:03:53 -0300

    desafioDioLogica

--- Object.js
+++ Object.js
@@ -1,10 +1,19 @@
-class formaDeBolo{
-  constructor(saborDaMassa, saborRecheio){
-    this.saborDaMassa = saborDaMassa
-    this.saborRecheio = saborRecheio
+class formaDeBolo {
+  constructor(saborDaMassa, saborRecheio) {
+    this.saborDaMassa = saborDaMassa;
+    this.saborRecheio = saborRecheio;
+  }
+
+  escrever() {
+    console.log(
+      `Um delicioso bolo de ${this.saborDaMassa} com recheio de ${this.saborRecheio}`
+    );
+    assar
   }
 }
 
-let boloFesta = new formaDeBolo("massa de chocolate", "Recheio de Nutela")
+let boloFesta = new formaDeBolo("chocolate", "Nutela");
+let boloPremium = new formaDeBolo("baunilha", "coco");
 
-console.log(boloFesta)
+boloFesta.escrever();
+boloPremium.escrever();
